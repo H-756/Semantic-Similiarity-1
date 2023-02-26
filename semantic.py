3b67bca96866fe84b6349790f398413764fa753a
H-756 <124478786+H-756@users.noreply.github.com>
2023-02-26 13:24:05 +0000

    Update semantic.py

--- semantic.py
+++ semantic.py
@@ -24,3 +24,8 @@ for sentence in sentences:
     similarity = nlp(sentence).similarity(model_sentence)
     print(sentence + " - ", similarity)
 
+# I have run the example file on both ‘en_core_web_sm’ and ‘en_core_web_md’ an i got the following message: 
+# UserWarning: [W007] The model you're using has no word vectors loaded, so the result of the Doc.similarity method will be based on the tagger, parser and NER, which may not give 
+# useful similarity judgements. This may happen if you're using one of the small models, e.g. `en_core_web_sm`, which don't ship with word vectors and only use context-sensitive tensors. 
+# You can always add your own word vectors, or use one of the larger models instead if available.
+# consequently i noticed a significant underestimation of similiarities between all factors when using `en_core_web_sm` compared to `en_core_web_md`.
